440933720182155ea77ab48a55d99525d3c60fc5
Mayank Mahra <mayankmahra1@gmail.com>
2026-09-03 16:30:19 +0100

    Rename intro file to Introduction

---
+++ Introduction.java
@@ -0,0 +1,23 @@
+public  class Introduction {
+
+    public static void main(String[] args) {
+
+        // A data structure is a named location that can be used to store and organise data 
+        // eg a family tree. We need some way to organise the names(data) of the family. A tree is a data structure
+        // An array is another eg. Its a collection of elements stored at contiguous memory locations. 
+
+
+        // Then whats an algoriithim. Its a collection of steps to solve a problem 
+        // An algorithim we are familiar with is a linear search
+        // Suppose we have an array of elements and I need to find a value. In a linear search we would examine 
+        // the elements of an array to see if we have that value 
+        // A linear search is an example of a searching algorithim 
+
+
+        // Why do we need to learn DSA. First you'll write code that is both time and memory efficient. 
+        // and in job interviews theyre common 
+
+        
+        
+    }
+}
